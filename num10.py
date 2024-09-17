2030570de321913639c31bcd16c21128c2e96a8d
Egor Karpunin <karpunin.es@phystech.edu>
2024-09-18 00:46:53 +0300

    Add files via upload

--- num10.py
+++ num10.py
@@ -1,17 +1,11 @@
-a=open('inputt.txt', encoding='utf-8')
 
-l=a.readlines()
-print(l)
-
-gl=['у', 'е', 'ы', 'а', 'о', 'э', 'ю', 'и', 'я']
-z=[]
-for g in l:
-    for h in g.split():
-        z.append(h)
-for i in range(len(z)):
-    for c in gl:
-        z[i].replace(c, )
-        
-a.close()
-        
-
+f = open('inputt.txt', 'r', encoding='utf-8')
+s = list(f.read())
+glas = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+sogl = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ъ']
+for i in range(len(s)):
+    if (i != 0):
+        if (s[i] in glas) and (s[i - 1] in sogl):
+            s[i] = str(s[i] + "с" + s[i])
+print(''.join(str(x) for x in s))
+f.close()
